3b3d69f4ee10b8572b931099d9748ca7b40330a7
Richard Cajucom <richard42smb@gmail.com>
2025-07-23 02:08:48 -0700

    Allow new Vercel URL in CORS whitelist

--- server.js
+++ server.js
@@ -9,17 +9,36 @@ const authRoutes = require('./routes/auth');
 
 const app = express();
 
-app.use(express.json());
 
+app.use(express.json());
 app.use(cookieParser());
 
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://flight-search-front-9zld12zj2-richards-projects-475ea311.vercel.app',
+  'https://flight-search-front-k4uwm1re8-richards-projects-475ea311.vercel.app'
+];
+
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "https://flight-search-front-9zld12zj2-richards-projects-475ea311.vercel.app",
+//     ],
+//     credentials: true,
+//   })
+// );
+
 app.use(cors({
-  origin: 'http://localhost:5173',  // your Rearct dev server
-  credentials: true
+  origin: allowedOrigins,
+  credentials: true,
 }));
 
+// Routes
 app.use("/flights", flightsRouter);
 app.use('/api/auth', authRoutes);
+
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -33,7 +52,3 @@ mongoose
     console.error("MongoDB connection error:", err);
     process.exit(1);
   });
-
-
-  
-
